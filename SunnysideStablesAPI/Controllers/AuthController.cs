7de2b6fe686f429c3f14ddcf3c07b6aa2fdfda19
marianneporter <mariannneporter17@gmail.com>
2021-06-13 17:24:03 +0100

    Login added

--- SunnysideStablesAPI/Controllers/AuthController.cs
+++ SunnysideStablesAPI/Controllers/AuthController.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SunnysideStablesAPI.Dtos;
+using SunnysideStablesAPI.Models.Identity;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SunnysideStablesAPI.Controllers
@@ -11,5 +19,83 @@ namespace SunnysideStablesAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _config;
+
+        public AuthController(UserManager<User> userManager,
+                              SignInManager<User> signInManager,
+                              IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);           
+
+            if (result.Succeeded)
+            {
+
+                var generatedToken = await GenerateJwtToken(user);
+
+                return Ok(new
+                {
+                    token = generatedToken,
+                    firstName = user.FirstName,
+                    lastName = user.LastName
+                });
+            }
+
+            return Unauthorized();
+
+        }
+
+        private async Task<string> GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            // Create new security key from config value, and then a new SigningCredentials object using this
+            // along with a hashing algorithm.
+            var jwtKeyFromConfig = _config.GetSection("JwtKey").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKeyFromConfig));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //Set up token descriptor and  create token with token handler
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
     }
 }
